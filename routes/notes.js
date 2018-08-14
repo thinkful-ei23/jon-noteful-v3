c7a723a834a0359895df06f1c38339106b89b532
jonsung89 <jonsung89@gmail.com>
2018-08-14 15:50:00 -0500

    updated routes/notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -53,24 +53,79 @@ router.get('/:id', (req, res, next) => {
 /* ========== POST/CREATE AN ITEM ========== */
 router.post('/', (req, res, next) => {
 
-  console.log('Create a Note');
-  res.location('path/to/new/document').status(201).json({ id: 2, title: 'Temp 2' });
+  const { title, content } = req.body;
+
+  if (!title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  const newItem = {
+    title: title,
+    content: content
+  };
+
+  Note
+    .create(newItem)
+    .then(result => {
+      if (result) {
+        res.json(result);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
 
 });
 
 /* ========== PUT/UPDATE A SINGLE ITEM ========== */
 router.put('/:id', (req, res, next) => {
 
-  console.log('Update a Note');
-  res.json({ id: 1, title: 'Updated Temp 1' });
+  const noteId = req.params.id;
+  const { title, content } = req.body;
 
+  if (!title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  const updateObj = {
+    title: title,
+    content: content
+  };
+
+  Note
+    .findByIdAndUpdate(noteId, { $set: updateObj }, { new: true })
+    .then(result => {
+      if (result) {
+        res.json(result);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
 });
+
 
 /* ========== DELETE/REMOVE A SINGLE ITEM ========== */
 router.delete('/:id', (req, res, next) => {
+  const id = req.params.id;
 
-  console.log('Delete a Note');
-  res.status(204).end();
+  Note
+    .findByIdAndRemove(id)
+    .then(() => {
+      res.sendStatus(204);
+    })
+    .catch(err => {
+      next(err);
+    });
+  
 });
 
 module.exports = router;
