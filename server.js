8579d66de41fc74839dd3965404f1fedec811a74
jonsung89 <jonsung89@gmail.com>
2018-08-14 15:01:18 -0500

    updated server.js to connect mongoose

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
-const { PORT } = require('./config');
+const { PORT, MONGODB_URI } = require('./config');
 
 const notesRouter = require('./routes/notes');
 
@@ -40,6 +41,24 @@ app.use((err, req, res, next) => {
     console.error(err);
     res.status(500).json({ message: 'Internal Server Error' });
   }
+});
+
+// Connect to DB and Listen for incoming connections
+mongoose.connect(MONGODB_URI)
+  .then(instance => {
+    const conn = instance.connections[0];
+    console.info(`Connected to: mongodb://${conn.host}:${conn.port}/${conn.name}`);
+  })
+  .catch(err => {
+    console.error(`ERROR: ${err.message}`);
+    console.error('\n === Did you remember to start `mongod`? === \n');
+    console.error(err);
+  });
+
+app.listen(PORT, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
 });
 
 // Listen for incoming connections
